c24cc96c5aa6113f4aa8861ba04a543942de1518
Aegon-n <Aegonn@protonmail.com>
2019-03-27 15:35:00 +0530

    added database

---
+++ handler/dbo/dbo.go
@@ -0,0 +1,31 @@
+package dbo
+
+import (
+	"github.com/syndtr/goleveldb/leveldb"
+	"log"
+)
+var ldb *leveldb.DB
+
+type LinkBot struct {}
+
+func  NewDB() {
+	db, err := leveldb.OpenFile("./database", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ldb = db
+	log.Println("Successfully initialized DB..")
+}
+
+func AddUserLang(username, lang string) error {
+	
+	return ldb.Put([]byte(username+"lang"),[]byte(lang), nil)
+
+}
+func GetUserLang(username string) string {
+	lang,err := ldb.Get([]byte(username+"lang"),nil)
+	if err != nil {
+		log.Fatal("user not found")
+	}
+	return string(lang)
+}
