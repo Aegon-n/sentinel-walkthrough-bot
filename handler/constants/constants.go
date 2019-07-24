ba25e4ab602bf86621c32e56e0259e79338cc51e
Aegon-n <Aegonn@protonmail.com>
2019-07-24 13:35:17 +0530

    modified

--- handler/constants/constants.go
+++ handler/constants/constants.go
@@ -3,7 +3,7 @@ package constants
 const (
 	DownloadUrl = "https://github.com/sentinel-official/sentinel/raw/master/docs/sentinel-MacOS-installation-guide.pdf"
 	//VideoUrl = "https://www.youtube.com/watch?v=Iaa7Jwd5sy8&t=7s"
-
+	ButtonsFilePath = "/root/go/src/github.com/Aegon-n/sentinel-bot/handler/buttons/buttons.json"
 	LinuxPdfUrl   = "https://github.com/sentinel-official/sentinel/raw/master/docs/sentinel-MacOS-installation-guide.pdf"
 	WindowsPdfUrl = "https://github.com/sentinel-official/sentinel/raw/master/docs/sentinel-MacOS-installation-guide.pdf"
 	MacPdfUrl     = "https://github.com/sentinel-official/sentinel/raw/master/docs/sentinel-MacOS-installation-guide.pdf"
@@ -14,5 +14,3 @@ const (
 	MacVideoUrl     = "https://youtu.be/Iaa7Jwd5sy8"
 	AndroidVideoUrl = "https://youtu.be/Iaa7Jwd5sy8"
 )
-
-var ButtonsFilePath = "/home/harish/go/src/github.com/Aegon-n/sentinel-bot/handler/buttons/buttons.json"
